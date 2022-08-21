a17ef42d2aebcac749e12c9e5b5fa8dd7658ed2c
votriet <92041076+votriet@users.noreply.github.com>
2022-08-21 16:09:10 +0700

    Add files via upload
    add chatbox-input css

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/HomePage.html');
+    res.sendFile(__dirname + 'Homepage.html');
 });
 io.on('connection', (socket) => {
     console.log('a user connected');
